2860c68f40e98b33750eaf86c8387e47100bba2a
1mpl3m3nt3d <105541001+1mpl3m3nt3d@users.noreply.github.com>
2022-10-14 23:25:59 +0300

    Upd

--- Infrastructure/Infrastructure/Extensions/HostingExtensions.cs
+++ Infrastructure/Infrastructure/Extensions/HostingExtensions.cs
@@ -1,9 +1,243 @@
 using Infrastructure.Configuration;
+using Infrastructure.Filters;
 
 namespace Infrastructure.Extensions;
 
 public static class HostingExtensions
 {
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            config.Sources.Clear();
+
+            var env = hostingContext.HostingEnvironment;
+
+            config.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath);
+
+            config.AddInMemoryCollection();
+
+            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            config.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+            if (env.IsDevelopment())
+            {
+                if (!string.IsNullOrEmpty(env.ApplicationName))
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                    if (appAssembly != null)
+                    {
+                        config.AddUserSecrets(appAssembly, optional: true);
+                    }
+                }
+            }
+
+            config.AddEnvironmentVariables(prefix: "ASPNETCORE_");
+
+            config.AddEnvironmentVariables();
+
+            var args = Environment.GetCommandLineArgs();
+
+            if (args != null)
+            {
+                config.AddCommandLine(args);
+            }
+        });
+
+        builder.AddConfiguration();
+
+        // 1st variant how to get desired configuration for WebApplicationBuilder in Program.cs
+        //var appConfig = new AppConfig();
+        //builder.Configuration.GetSection(AppConfig.App).Bind(appConfig);
+
+        // 2nd variant how to get desired configuration for WebApplicationBuilder in Program.cs
+        var appConfig = builder.Configuration.GetSection(AppConfig.App).Get<AppConfig>();
+        var authConfig = builder.Configuration.GetSection(AuthorizationConfig.Authorization).Get<AuthorizationConfig>();
+
+        builder.Services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders =
+                ForwardedHeaders.XForwardedFor |
+                ForwardedHeaders.XForwardedHost |
+                ForwardedHeaders.XForwardedProto;
+            options.ForwardLimit = 2;
+            options.RequireHeaderSymmetry = false;
+
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
+
+        builder.Services.AddCertificateForwarding(options => { });
+
+        builder.Services.AddHsts(options =>
+        {
+            options.IncludeSubDomains = true;
+            options.MaxAge = TimeSpan.FromDays(60);
+            options.Preload = true;
+        });
+
+        builder.Services.AddHttpsRedirection(options =>
+        {
+            options.RedirectStatusCode = (int)HttpStatusCode.TemporaryRedirect;
+
+            var httpsPort = builder.Configuration["HTTPS_PORT"] ?? Environment.GetEnvironmentVariable("HTTPS_PORT");
+
+            var isPortParsed = int.TryParse(httpsPort, out var portParsed);
+
+            if (isPortParsed)
+            {
+                options.HttpsPort = portParsed;
+            }
+        });
+
+        builder.Services.AddCookiePolicy(options =>
+        {
+            options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.None;
+            options.MinimumSameSitePolicy = SameSiteMode.None;
+            options.Secure = CookieSecurePolicy.SameAsRequest;
+        });
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.HttpOnly = false;
+            options.Cookie.Expiration = TimeSpan.FromDays(30);
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+            options.ExpireTimeSpan = TimeSpan.FromDays(30);
+            options.SlidingExpiration = true;
+        });
+
+        builder.Services.ConfigureExternalCookie(options =>
+        {
+            options.Cookie.HttpOnly = false;
+            options.Cookie.Expiration = TimeSpan.FromDays(30);
+            options.Cookie.SameSite = SameSiteMode.None;
+            options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+            options.ExpireTimeSpan = TimeSpan.FromDays(30);
+            options.SlidingExpiration = true;
+        });
+
+        builder.Services.AddCors(
+            options => options
+                .AddPolicy(
+                    "CorsPolicy",
+                    corsBuilder => corsBuilder
+                        //.SetIsOriginAllowed((host) => true)
+                        .WithOrigins(
+                            authConfig.Authority,
+                            appConfig.BaseUrl,
+                            appConfig.GlobalUrl,
+                            appConfig.SpaUrl)
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()));
+
+        builder.Services
+            .AddControllers(options => options.Filters.Add(typeof(HttpGlobalExceptionFilter)))
+            .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
+
+        builder.AddHttpLoggingConfiguration();
+        builder.AddNginxConfiguration();
+        builder.AddAuthorization();
+
+        return builder;
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // a variant how to get desired configuration for WebApplication in Program.cs
+        var webAppConfig = app.Services.GetRequiredService<IOptionsMonitor<AppConfig>>().CurrentValue;
+
+        var basePath = webAppConfig.BasePath;
+
+        if (!string.IsNullOrEmpty(basePath))
+        {
+            app.UsePathBase(basePath);
+        }
+
+        if (webAppConfig.HttpLogging == "true")
+        {
+            app.UseHttpLogging();
+
+            app.Use(async (ctx, next) =>
+            {
+                var remoteAddress = ctx.Connection.RemoteIpAddress;
+                var remotePort = ctx.Connection.RemotePort;
+
+                app.Logger.LogInformation($"Request Remote: {remoteAddress}:{remotePort}");
+
+                await next(ctx);
+            });
+        }
+
+        var forwardedHeadersOptions = new ForwardedHeadersOptions()
+        {
+            ForwardedHeaders =
+                ForwardedHeaders.XForwardedFor |
+                ForwardedHeaders.XForwardedHost |
+                ForwardedHeaders.XForwardedProto,
+            ForwardLimit = 2,
+            RequireHeaderSymmetry = false,
+        };
+
+        forwardedHeadersOptions.KnownNetworks.Clear();
+        forwardedHeadersOptions.KnownProxies.Clear();
+
+        app.UseForwardedHeaders(forwardedHeadersOptions);
+
+        app.UseCertificateForwarding();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+
+            // The default HSTS value is 30 days.
+            // see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+            app.UseHttpsRedirection();
+        }
+
+        //app.UseDefaultFiles();
+        //app.UseStaticFiles();
+
+        var cookiePolicyOptions = new CookiePolicyOptions()
+        {
+            HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.None,
+            MinimumSameSitePolicy = SameSiteMode.None,
+            Secure = CookieSecurePolicy.SameAsRequest,
+        };
+
+        app.UseCookiePolicy(cookiePolicyOptions);
+
+        app.UseRouting();
+
+        //app.UseRequestLocalization();
+
+        app.UseCors("CorsPolicy");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        //app.UseSession();
+        //app.UseResponseCompression();
+        //app.UseResponseCaching();
+
+        //app.MapControllers();
+        //app.MapDefaultControllerRoute();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapDefaultControllerRoute();
+        });
+
+        return app;
+    }
+
     public static WebApplicationBuilder AddNginxConfiguration(this WebApplicationBuilder builder)
     {
         var appConfig = builder.Configuration.GetSection(AppConfig.App).Get<AppConfig>();
